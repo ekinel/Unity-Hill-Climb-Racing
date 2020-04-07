c1ae19fecfe771a29da439dc1b0dea60ae71e03d
Elena Kiseleva <kiseleva.e.98@gmail.com>
2020-04-07 18:17:18 +0700

    Frst script has been added
    Car and conrol scripts has been created

---
+++ Hill-Climb-Racing/Assets/Scripts/CarScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour
+{
+	WheelJoint2D[] wheelJoints;
+	JointMotor2D frontWheel, backWheel;
+
+	public float maxSpeed = -1000f;
+	private float maxBackSpeed = 1500f;
+	private float acceleration = 250f, deacceleration = -100f;
+	public float brakeForce = 3000f;
+	private float gravity = 9.8f, angle = 0;
+
+	public ClickScript[] CarControl;
+
+    void Start()
+    {
+		wheelJoints = gameObject.GetComponents<WheelJoint2D>();
+		frontWheel = wheelJoints[0].motor;
+		backWheel = wheelJoints[1].motor;
+    }
+
+    void Update()
+    {
+		angle = transform.localEulerAngles.z;
+        
+    }
+}
---
+++ Hill-Climb-Racing/Assets/Scripts/ClickScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickScript : MonoBehaviour
+{
+	private bool isClicked = false;
+
+	private void OnMouseDown()
+	{
+		isClicked = true;
+	}
+
+	private void OnMouseUp()
+	{
+		isClicked = false;
+	}
+}
